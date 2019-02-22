6e62ba8ad4515f1514ea5c45144b2c05a74f78d4
aliciaskutle <45006675+aliciaskutle@users.noreply.github.com>
2019-02-22 11:04:46 +0100

    Add files via upload

---
+++ level1/question1/script.js
@@ -0,0 +1,5 @@
+//Alert when button is clicked
+
+document.getElementById("myButton").addEventListener("click", function() {
+  alert("The alert is working!");
+});
---
+++ level1/question2/script.js
@@ -0,0 +1,1 @@
+//Level 2 - Question 2
