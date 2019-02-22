a251fc4ab526b363e544430a42171fa7783ce181
Alicia Skutle <alicia.skutle@gmail.com>
2019-02-22 23:13:00 +0100

    2

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,17 @@
 //Level 2 - Question 2
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(todos => createTodos(todos))
+  .catch(err => console.log(err));
+
+function createTodos(todos) {
+  var todoList = document.getElementById("todoList");
+
+  for (var todo of todos) {
+    todoList.innerHTML += `
+    <div class="todo">
+      <span class="todo-thing">Title: ${todo.title}</span>
+      <span class="todo-thing">Completed: ${todo.completed}</span>
+    </div>`;
+  }
+}
